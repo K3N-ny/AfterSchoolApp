63741cd1f23f50b010ac9936669963b3b59f9bf6
kenny orobogha <KO458@live.mdx.ac.uk>
2024-10-16 05:44:09 +0100

    "Update README.md with new content and formatting"

---
+++ src/App.vue
@@ -0,0 +1,29 @@
+<template>
+ <div id="app">
+  <div class="container">
+    <div class="row">
+      <p> Subject: {{ subject }}</p>
+      <br>
+      <p> Location: {{ location }}</p>
+      <br>
+      <p> Price: {{ price }}</p>
+      <br>
+      <p> Spaces: {{ spaces }}</p>
+    </div>
+  </div>
+ </div>
+ 
+</template>
+
+<script>
+ export  default {
+    data() {
+      return {
+        subject:  'Maths',
+        location: 'London',
+        price: 100,
+        spaces: 5,
+      }
+    }
+  }
+</script>
